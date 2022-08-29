6ea3cffdc1c474e72faa0a2a18fa4be349389dbb
bxljoy <259500313@qq.com>
2022-08-29 20:22:25 +0800

    build an array with stack operations

---
+++ src/com/alexbao/leetcode8/BuildAnArrayWithStackOperations.java
@@ -0,0 +1,23 @@
+package com.alexbao.leetcode8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BuildAnArrayWithStackOperations {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> res = new ArrayList<>();
+        int pointer = 0;
+        int num = 1;
+        while (pointer < target.length) {
+            res.add("Push");
+            if (num == target[pointer]) {
+                pointer++;
+            } else {
+                res.add("Pop");
+            }
+            num++;
+        }
+        return res;
+    }
+}
