e7022c6a567b88ff76e6e4e4cd73d9b33d568318
bxljoy <259500313@qq.com>
2022-09-01 23:08:56 +0800

    subtree of another tree

---
+++ src/com/alexbao/leetcode8/SubtreeOfAnotherTree.java
@@ -0,0 +1,18 @@
+package com.alexbao.leetcode8;
+
+public class SubtreeOfAnotherTree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return subRoot == null;
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
