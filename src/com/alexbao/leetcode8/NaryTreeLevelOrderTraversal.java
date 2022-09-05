55004bdf3b50a04ad610858a986fc7462e394293
bxljoy <259500313@qq.com>
2022-09-06 00:13:16 +0800

    N-ary Tree level order traversal

---
+++ src/com/alexbao/leetcode8/NaryTreeLevelOrderTraversal.java
@@ -0,0 +1,48 @@
+package com.alexbao.leetcode8;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class NaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i=0; i<size; i++) {
+                Node cur = queue.poll();
+                list.add(cur.val);
+                if (cur.children != null) {
+                    for (Node child : cur.children) {
+                        queue.offer(child);
+                    }
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+
